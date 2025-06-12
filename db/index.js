bec0d57930a58e3b4f273e751fcd011e0586063d
nt1538 <nt1538@nyu.edu>
2025-06-11 23:13:50 -0400

    backend

--- db/index.js
+++ db/index.js
@@ -2,12 +2,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'hierarchy_db',
-  password: process.env.DB_PASSWORD || 'Access121',
-  port: process.env.DB_PORT || 5432,
-  ssl: { rejectUnauthorized: false }
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'hierarchy_db',
+    password: process.env.DB_PASSWORD || 'Access121',
+    port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
