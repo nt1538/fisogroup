b30fde2127401bc4f1a7143c3aa17c1c7988c3fa
nt1538 <nt1538@nyu.edu>
2025-06-11 23:16:35 -0400

    backend

--- db/index.js
+++ db/index.js
@@ -7,6 +7,8 @@ const pool = new Pool({
     database: process.env.DB_NAME || 'hierarchy_db',
     password: process.env.DB_PASSWORD || 'Access121',
     port: process.env.DB_PORT || 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 module.exports = pool;
