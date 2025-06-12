11dbcbb65af3e6185ca7bfa92e437ce29a306e12
nt1538 <nt1538@nyu.edu>
2025-06-11 23:22:34 -0400

    backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+require('dotenv').config();
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
 const orderRoutes = require('./routes/orders');
