fbfc95bcafb14f5c0f9426bd89738a293494537f
nt1538 <nt1538@nyu.edu>
2025-06-15 12:25:44 -0400

    db

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS life_orders;
 DROP TABLE IF EXISTS applications;
 DROP TABLE IF EXISTS documents;
 DROP TABLE IF EXISTS uploads;
-DROP TABLE IF EXISTS annuities;
+DROP TABLE IF EXISTS annuities_orders;
 
 -- USERS TABLE (with state info)
 CREATE TABLE users (
@@ -81,7 +81,7 @@ CREATE TABLE uploads (
 );
 
 -- ANNUITIES TABLE (Matches full frontend display)
-CREATE TABLE annuities (
+CREATE TABLE annuities_orders (
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id),
   agent_fiso TEXT,
