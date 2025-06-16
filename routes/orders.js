9b51a71ba9523b7220c013bac2637e2e18c4d46d
nt1538 <nt1538@nyu.edu>
2025-06-16 01:30:05 -0400

    order

--- routes/orders.js
+++ routes/orders.js
@@ -87,25 +87,49 @@ async function createOrder(req, res, tableName, defaultType) {
 
     // Insert main order (current user)
     const insertRes = await client.query(
-      `INSERT INTO ${tableName}
-        (user_id, company, policy_number, amount, state, date, order_type, commission_percent, commission_amount, chart_percent, level_percent, application_status)
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
-       RETURNING id`,
-      [
-        user_id,
-        company,
-        policy_number,
-        amount,
-        state?.toUpperCase() || null,
-        date,
-        order_type,
-        actual_percent,
-        commission_amount,
-        chart_percent,
-        user.level_percent,
-        application_status,
-      ]
-    );
+  `INSERT INTO ${tableName}
+    (user_id, company, policy_number, amount, state, date, order_type, commission_percent, commission_amount,
+     chart_percent, level_percent, application_status,
+     agent_fiso, first_name, last_name, national_producer_number, license_number, hierarchy_level, split_percent,
+     carrier_name, product_type, product_name_carrier, application_date, face_amount, target_premium, initial_premium,
+     commission_from_carrier, mra_status)
+   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,
+           $10, $11, $12,
+           $13, $14, $15, $16, $17, $18, $19,
+           $20, $21, $22, $23, $24, $25, $26,
+           $27, $28)
+   RETURNING id`,
+[
+  user_id,
+  company,
+  policy_number,
+  amount,
+  state?.toUpperCase() || null,
+  date,
+  order_type,
+  actual_percent,
+  commission_amount,
+  chart_percent,
+  user.level_percent,
+  application_status,
+  req.body.agent_fiso,
+  req.body.first_name,
+  req.body.last_name,
+  req.body.national_producer_number,
+  req.body.license_number,
+  req.body.hierarchy_level,
+  req.body.split_percent || 100,
+  req.body.carrier_name,
+  req.body.product_type,
+  req.body.product_name_carrier,
+  req.body.application_date,
+  req.body.face_amount,
+  req.body.target_premium,
+  req.body.initial_premium,
+  req.body.commission_from_carrier,
+  req.body.mra_status || 'none'
+]
+);
 
     const orderId = insertRes.rows[0].id;
 
