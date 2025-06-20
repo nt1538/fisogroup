6473e1110f241f9297e838146670a8198a6e1dc1
nt1538 <nt1538@nyu.edu>
2025-06-20 13:21:20 -0400

    flex

--- web/src/layout/src/Layout.vue
+++ web/src/layout/src/Layout.vue
@@ -1,39 +1,48 @@
 <template>
-  <div class="my-layout" :class="{ m: isMClient }">
-    <MY-Body>
-      <MY-Header />
-      <MY-Content>
-        <slot />
-      </MY-Content>
-    </MY-Body>
+  <div class="app-layout">
+    <Sidebar class="sidebar" />
+    <div class="main">
+      <Header class="header" />
+      <router-view />
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-  import { useStore } from 'vuex'
-  import { defineAsyncComponent, computed } from 'vue'
-  const MYBody = defineAsyncComponent(() => import('./components/Body.vue'))
-  const MYContent = defineAsyncComponent(() => import('./components/Content.vue'))
-  const MYHeader = defineAsyncComponent(() => import('./components/Header.vue'))
-
-  const store = useStore()
-
-  const isMClient = computed(() => store.state.common.isM)
+<script setup>
+import Sidebar from '@/components/Sidebar.vue'
+import Header from '@/layout/src/components/Header.vue'
 </script>
-<style lang="scss">
-  @include b(layout) {
-    position: relative;
-    height: 100%;
-    width: 100%;
 
-    @include pseudo(after) {
-      content: '';
-      display: table;
-      clear: both;
-    }
-  }
+<style scoped>
+.app-layout {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+}
 
-  @include b(page) {
-    margin: 10px;
-  }
+.sidebar {
+  width: 200px;
+  background: #f5f5f5;
+}
+
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.header {
+  height: 80px;
+  flex-shrink: 0;
+  background-color: #fff;
+  z-index: 100;
+}
+
+router-view {
+  flex: 1;
+  overflow: auto;
+}
 </style>
+
