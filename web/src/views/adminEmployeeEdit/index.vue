955778c22ca337f29e93f526ac1ee42212784752
nt1538 <nt1538@nyu.edu>
2025-06-30 15:41:20 -0400

    admin

--- web/src/views/adminEmployeeEdit/index.vue
+++ web/src/views/adminEmployeeEdit/index.vue
@@ -44,7 +44,15 @@
 
       <div class="form-group">
         <label>Hierarchy Level</label>
-        <input v-model="user.hierarchy_level" type="text" />
+        <select v-model="user.hierarchy_level">
+          <option value="Level A">Level A</option>
+          <option value="Level B">Level B</option>
+          <option value="Level C">Level C</option>
+          <option value="Agency 1">Agency 1</option>
+          <option value="Agency 2">Agency 2</option>
+          <option value="Agency 3">Agency 3</option>
+          <option value="Vice President">Vice President</option>
+        </select>
       </div>
 
       <div class="form-group">
