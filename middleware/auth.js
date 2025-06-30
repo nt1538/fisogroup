2cb16a89835aa258ed23a38203f36969d122d5b3
nt1538 <nt1538@nyu.edu>
2025-06-30 10:45:31 -0400

    admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,11 +17,12 @@ function verifyToken(req, res, next) {
 }
 
 function verifyAdmin(req, res, next) {
-  if (!req.user.is_admin) {
+  if (!req.user || req.user.is_admin !== true) {
     return res.status(403).json({ error: 'Access denied: Admins only' });
   }
   next();
 }
+
 
 module.exports = {
   verifyToken,
