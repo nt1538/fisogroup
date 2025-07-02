baeca40bfc71f589cbc5fb6418e7dbee28af8ef2
nt1538 <nt1538@nyu.edu>
2025-07-02 11:45:28 -0400

    admin

--- web/src/views/OrgChart/TreeNode.vue
+++ web/src/views/OrgChart/TreeNode.vue
@@ -1,27 +1,36 @@
 <template>
-  <li>
-    <div class="node">
-      {{ node.full_name }}（{{ node.hierarchy_level }}）
+  <div class="node">
+    <div class="node-info">
+      {{ node.name }} - {{ node.hierarchy_level }}
     </div>
-    <ul v-if="node.children && node.children.length">
-      <TreeNode
-        v-for="child in node.children"
-        :key="child.id"
-        :node="child"
-      />
-    </ul>
-  </li>
+
+    <!-- 子节点递归 -->
+    <div class="children" v-if="node.children && node.children.length">
+      <TreeNode v-for="child in node.children" :key="child.id" :node="child" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-defineProps({
-  node: Object
-});
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({ node: Object })
+
 </script>
 
 <style scoped>
 .node {
-  font-weight: 500;
-  margin: 4px 0;
+  margin: 10px 0;
+}
+.node-info {
+  padding: 8px 12px;
+  background: #f0f8ff;
+  border-left: 3px solid #0055a4;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+}
+.children {
+  padding-left: 20px;
 }
 </style>
