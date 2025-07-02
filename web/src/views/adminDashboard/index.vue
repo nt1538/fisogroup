ee264201e974bcd69685b89d928833067ad33d8e
nt1538 <nt1538@nyu.edu>
2025-07-02 17:13:34 -0400

    admin

--- web/src/views/adminDashboard/index.vue
+++ web/src/views/adminDashboard/index.vue
@@ -2,10 +2,10 @@
   <AdminLayout>
     <h2>Dashboard</h2>
     <div class="grid">
-      <div class="card">User Count：{{ stats.userCount }}</div>
-      <div class="card">Life Order Count：{{ stats.lifeOrderCount }}</div>
-      <div class="card">Annuity Order Count：{{ stats.annuityOrderCount }}</div>
-      <div class="card">Total Commission：${{ stats.totalCommissionAmount }}</div>
+      <div class="card">User Count: {{ stats.userCount }}</div>
+      <div class="card">Life Order Count: {{ stats.lifeOrderCount }}</div>
+      <div class="card">Annuity Order Count: {{ stats.annuityOrderCount }}</div>
+      <div class="card">Total Commission: ${{ stats.totalCommissionAmount }}</div>
     </div>
   </AdminLayout>
 </template>
