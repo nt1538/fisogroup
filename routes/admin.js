4bfe0d4a298ee01a5109083016e737227ec82de0
nt1538 <nt1538@nyu.edu>
2025-07-03 11:53:00 -0400

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -177,17 +177,17 @@ router.delete('/orders/:type/:id', async (req, res) => {
 // 👤 编辑员工信息
 router.put('/employees/:id', verifyToken, verifyAdmin, async (req, res) => {
   const { id } = req.params;
-  const {name, email, state, introducer_id, level_percent, total_earnings, commission, hierarchy_level, national_producer_number} = req.body;
+  const {name, email, state, introducer_id, level_percent, total_earnings, commission, profit, hierarchy_level, national_producer_number} = req.body;
 
   try {
     const query = `
       UPDATE users
-      SET name = $1, email = $2, state = $3, introducer_id = $4, level_percent = $5, total_earnings = $6, commission = $7, 
-      hierarchy_level = $8, national_producer_number = $9
-      WHERE id = $10
+      SET name = $1, email = $2, state = $3, introducer_id = $4, level_percent = $5, total_earnings = $6, commission = $7, profit = $8,
+      hierarchy_level = $9, national_producer_number = $10
+      WHERE id = $11
       RETURNING *;
     `;
-    const values = [name, email, state, introducer_id, level_percent, total_earnings, commission, hierarchy_level, national_producer_number, id];
+    const values = [name, email, state, introducer_id, level_percent, total_earnings, commission, profit, hierarchy_level, national_producer_number, id];
 
     const result = await pool.query(query, values);
     if (result.rowCount === 0) {
